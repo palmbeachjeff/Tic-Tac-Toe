f1828e2fbe560657b0d37a61c0e33d7ce42c3dac
palmbeachjeff <palmbeachjeff@gmail.com>
2015-04-23 11:07:42 -0400

    passed tests for move_possiblemoves_win

--- lib/position.rb
+++ lib/position.rb
@@ -9,4 +9,26 @@ class Position
   @board = board || %w(-)*SIZE
   @turn = turn
   end
+
+  def move(index)
+    position = Position.new(@board.dup, xturn("o", "x"))
+    position.board[index] = turn
+    position
+  end
+
+  def xturn(x,o)
+    turn == "x" ? x : o
+  end
+
+  def possible_moves
+    @board.map.with_index { |p,i| i if p == "-"}.compact
+  end
+
+  def win?(turn)
+    
+  end
+
+
+
+
 end
--- spec/lib/position_spec.rb
+++ spec/lib/position_spec.rb
@@ -8,7 +8,26 @@ describe Position do
     subject {Position.new(%w(x - - - - - - - - ), "o") }
     its(:board) { should == %w(x - - - - - - - -) }
     its(:turn) { should == "o"}
+  end
 
+  context "#move" do
+    subject { Position.new.move(0) }
+    its(:board) { should == %w(x - - - - - - - -) }
+    its(:turn) { should == "o"}
+  end
+
+  context "#possible_moves" do
+    subject { Position.new.move(0).move(1) }
+    its(:possible_moves) {should == [2,3,4,5,6,7,8] }
   end 
+
+  context "#win?" do
+    it {expect(Position.new.win?("x")).to be_falsey}
+  end
+
+
+
+
+
 end
   
