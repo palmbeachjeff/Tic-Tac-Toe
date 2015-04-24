5a97286fe13c615623b83a0f09107b1dfbd3ef90
palmbeachjeff <palmbeachjeff@gmail.com>
2015-04-24 12:24:34 -0400

    almost done, fixing last few bugs

--- lib/game.rb
+++ lib/game.rb
@@ -6,18 +6,25 @@ class Game < Gosu::Window
 
   def initialize
     @width = 600
-    @cell_width = @width/
+    @cell_width = @width/3
     @position = Position.new
-    @font = Gosu::Font.new(self, Gosu::default_font_name, cell_width)
     super(width, width, false)
+    @font = Gosu::Font.new(self, Gosu::default_font_name, cell_width)
+    @mfont = Gosu::Font.new(self, Gosu::default_font_name, cell_width/3) 
   end
 
 
   def button_down(id)
   case id
   when Gosu::KbQ then close
-  when Gosu::MsLeft then @position = @position.move((mouse_x/cell_width).to_i + 3*(mouse_y/cell_width).to_i)
+  when Gosu::MsLeft then 
+    @position = @position.move((mouse_x/cell_width).to_i + 3*(mouse_y/cell_width).to_i)
+    if @position.possible_moves.empty? then
+      index = @position.best_move
+      @position = @position.move(index)
+    end
   end
+end
 
 
   def needs_cursor?
@@ -26,18 +33,34 @@ class Game < Gosu::Window
 
   def draw
     [cell_width, cell_width*2].each do |w|
-    draw_line(w, 0, Gosu::Color::WHITE, w, width, Gosu::Color::White)
-    draw_line(0, w, Gosu::Color::WHITE, width, w, Gosu::Color::White)
+    draw_line(w, 0, Gosu::Color::WHITE, w, width, Gosu::Color::WHITE)
+    draw_line(0, w, Gosu::Color::WHITE, width, w, Gosu::Color::WHITE)
   end
 
   
   position.board.each.with_index do |p,i|
     if p != "-"
-      x = (i%3)*cell_width
+      x = (i%3)*cell_width+@font.text_width(p)/2
       y = (i/3)*cell_width
       @font.draw(p, x, y, 0)
       end
     end
+
+    display_message("You won") if @position.win?("x")
+    display_message("Computer won") if @position.win?("o")
+    display_message("Draw") if @position.possible_moves.empty?
+  end
+
+  def display_message(txt)
+    black = Gosu::Color::BLACK
+    draw_quad(0,100,black, 
+      width, 100, black,
+      width, 500, black,
+      0, 500, black )
+
+    @mfont.draw(txt(width-@mfont.text_width(txt))/2, width/2-100, 0)
+
+
   end
 
 end
